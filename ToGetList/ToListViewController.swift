55b3c581c522db0094f20549d63664d6661a95a9
kartik yadav <iamkrt@yahoo.in>
2018-08-08 14:53:16 +0530

    To do list completed with data source and delegates methods.

---
+++ ToGetList/ToListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ToGetList
+//
+//  Created by kartik yadav on 06/08/18.
+//  Copyright © 2018 iamkrt. All rights reserved.
+//
+
+import UIKit
+
+class ToListViewController: UITableViewController {
+
+    let itemArray = ["Hey" , "boy" , "its" , "KRT"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+
+}
+
